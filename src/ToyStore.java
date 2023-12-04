88b8da1a461a78463c315b7b329debc269b27aa0
Al Vin <zdejh.gm@gmail.com>
2023-12-04 09:23:17 +0300

    add toystore w full functions

---
+++ src/ToyStore.java
@@ -0,0 +1,63 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ToyStore {
+    private List<Toy> toys;
+    private List<Toy> prizes;
+
+    public ToyStore(){
+        toys = new ArrayList<>();
+        prizes = new ArrayList<>();
+
+    }
+    public void addToy(int id, String toyName, int quantity, double dropFrequency){
+        Toy toy = new Toy (id, toyName, quantity, dropFrequency);
+        toys.add(toy);
+    }
+    public void setDropFrequency (int id, double dropFrequency){
+        for (Toy toy : toys){
+            if (toy.getId() == id){
+                toy.setDropFrequency(dropFrequency);
+                break;
+            }
+        }
+    }
+
+    public void getPrize() throws IOException {
+        if (prizes.size() > 0) {
+            Toy prize = prizes.remove(0);
+
+            FileWriter writer = new FileWriter("prize.log", true);
+            writer.write(prize.getId() + ";" + prize.getToyName()+"\n");
+            writer.close();
+        }
+    }
+
+    public void play()
+    {
+        double totalFrequency = 0;
+        for (Toy toy : toys){
+            totalFrequency += toy.getDropFrequency();
+        }
+
+        Random rnd = new Random();
+        double rndNumber = rnd.nextDouble() * totalFrequency;
+
+        Toy prize = null;
+        for (Toy toy :toys){
+            if (rndNumber < toy.getDropFrequency()){
+                prize = toy;
+                break;
+            }
+            rndNumber -= toy.getDropFrequency();
+        }
+
+        if (prize != null && prize.getQuantity() >0 ){
+            prizes.add(prize);
+            prize.setQuantity((prize.getQuantity()) - 1);
+        }
+    }
+}
