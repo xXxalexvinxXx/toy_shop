c3202875f258e07ee41f64b672deaba19eacf2dc
Al Vin <zdejh.gm@gmail.com>
2023-12-04 08:49:14 +0300

    some changes

--- src/Toy.java
+++ src/Toy.java
@@ -1,15 +1,13 @@
-package Model;
-
-import javax.swing.*;
-
 public class Toy {
-private int id;
-private String toyName;
-private int dropFrequency;
+    private int id;
+    private String toyName;
+    private int quantity;
+    private double dropFrequency;
 
-    public Toy(int id, String toyName, int dropFrequency) {
+    public Toy(int id, String toyName, int quantity, double dropFrequency) {
         this.id = id;
         this.toyName = toyName;
+        this.quantity = quantity;
         this.dropFrequency = dropFrequency;
     }
 
@@ -29,11 +27,19 @@ private int dropFrequency;
         this.toyName = toyName;
     }
 
-    public int getDropFrequency() {
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getDropFrequency() {
         return dropFrequency;
     }
 
-    public void setDropFrequency(int dropFrequency) {
+    public void setDropFrequency(double dropFrequency) {
         this.dropFrequency = dropFrequency;
     }
 }
